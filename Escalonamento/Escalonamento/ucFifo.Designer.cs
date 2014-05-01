151de326be517eee0a0243a494e40cf86b1daf8c
henriquefeil <henriquefeil@gmail.com>
2014-05-01 00:40:46 -0300

    Ajustei o adiciona processos. Falta somente o Executar.

--- Escalonamento/Escalonamento/ucFifo.Designer.cs
+++ Escalonamento/Escalonamento/ucFifo.Designer.cs
@@ -48,79 +48,39 @@
             // dgvProcessos
             // 
             this.dgvProcessos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvProcessos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5,
-            this.Column6,
-            this.Column7,
-            this.Column8});
             this.dgvProcessos.Location = new System.Drawing.Point(4, 86);
             this.dgvProcessos.Name = "dgvProcessos";
             this.dgvProcessos.Size = new System.Drawing.Size(854, 272);
             this.dgvProcessos.TabIndex = 7;
             // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Processo";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Prioridade";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Prioridade Default";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "TempoExecuçãoDefault";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Tempo Execução";
-            this.Column5.Name = "Column5";
-            // 
-            // Column6
-            // 
-            this.Column6.HeaderText = "Início";
-            this.Column6.Name = "Column6";
-            // 
-            // Column7
-            // 
-            this.Column7.HeaderText = "Ativo";
-            this.Column7.Name = "Column7";
-            // 
-            // Column8
-            // 
-            this.Column8.HeaderText = "Fim";
-            this.Column8.Name = "Column8";
+
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.cboTempoExecucao);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Location = new System.Drawing.Point(6, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(855, 76);
+            this.groupBox1.Size = new System.Drawing.Size(845, 77);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Adicionar Processo";
             // 
             // cboTempoExecucao
             // 
+            this.cboTempoExecucao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboTempoExecucao.FormattingEnabled = true;
-            this.cboTempoExecucao.Location = new System.Drawing.Point(129, 33);
+            this.cboTempoExecucao.Items.AddRange(new object[] {
+            "10",
+            "8",
+            "6"});
+            this.cboTempoExecucao.Location = new System.Drawing.Point(116, 23);
             this.cboTempoExecucao.Name = "cboTempoExecucao";
-            this.cboTempoExecucao.Size = new System.Drawing.Size(114, 21);
+            this.cboTempoExecucao.Size = new System.Drawing.Size(121, 21);
             this.cboTempoExecucao.TabIndex = 4;
+
+
             // 
             // button1
             // 
@@ -130,15 +90,19 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Adicionar Processo";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnAdicionarProcesso_Click);
+
+
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 36);
+            this.label1.Location = new System.Drawing.Point(6, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 1;
             this.label1.Text = "Tempo de Execução";
+
             // 
             // ucFifo
             // 
